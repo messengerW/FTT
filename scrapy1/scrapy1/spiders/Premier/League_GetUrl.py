5464d86a8436f4f4bd11543fa90217f68a7ec60d
messengerW <messenger232624@gmail.com>
2019-05-26 21:00:01 +0800

    update

--- scrapy1/scrapy1/spiders/Premier/League_GetUrl.py
+++ scrapy1/scrapy1/spiders/Premier/League_GetUrl.py
@@ -2,35 +2,34 @@
 # 这个类爬到的 urls 写进 excel 里面 然后再在需要的类里面读取
 # 使用的话，只需要把目录下的原来的urls.xls清空，然后把 下面 start_urls 地址的数字改一下就可以了
 # 还在不断完善中···
+# 我宣布...xlrd 还有 xlwt 淘汰辽
+# 注意，这个 xlwings 下标从1开始，第一个单元格 (1,1)
 import scrapy
-import xlrd
-import os
-from xlutils.copy import copy
+import xlwings as xw
 
-filepath = "C:/Users/mushr/Desktop/433/urls.xls"  # 要保存的地址及文件名
-
+filepath = "C:/Users/mushr/Desktop/433/urls.xlsx"  # 要保存的地址及文件名
 
 class GetUrl(scrapy.Spider):
     name = 'spider_geturls'
 
     allowed_domains = ['tzuqiu.cc']
 
-    start_urls = ['http://www.tzuqiu.cc/teams/16/fixture.do', ]
+    start_urls = ['http://www.tzuqiu.cc/teams/207/fixture.do', ]
 
     def parse(self, response):
         # 打开一个workbook
-        rb = xlrd.open_workbook(filepath)  # readbook
-        wb = copy(rb)  # writebook
-        # 获取sheet对象，通过sheet_by_index()获取的sheet对象没有write()方法
-        sheet = wb.get_sheet(0)
+        app = xw.App(visible=False, add_book=False)
+        wb = app.books.open(filepath)
+        sheet = wb.sheets['sheet1']
 
         # 获取当前页面的球队，即打开的是哪个球队的赛程板
-        mpage_team = response.xpath("//div[@class='container mainfdb']/div[1]/div[1]/div[1]/div[1]/h1/text()").extract_first()
+        mpage_team = response.xpath(
+            "//div[@class='container mainfdb']/div[1]/div[1]/div[1]/div[1]/h1/text()").extract_first()
         mpage_team = "".join(mpage_team.split())
 
         # 获取到当前页面 table 下面所有的 tr，存入列表
         tr_list = response.xpath("//table[@class='fiture team-fixture']//tbody[@id='fixture-body']/tr")
-        print("========>>>> %s 一共有 %d 条比赛信息" %(mpage_team, len(tr_list)))
+        print("========>>>> %s 一共有 %d 条比赛信息" % (mpage_team, len(tr_list)))
         for i in tr_list:  # 注意这里的 i 可不是数字 而是一个个的 xpath
             # 获取到比赛性质（欧冠、联赛、足总杯...）
             game_properties = i.xpath(".//td[3]/a/@title").extract_first()
@@ -42,35 +41,30 @@ class GetUrl(scrapy.Spider):
                 next_url = 'http://www.tzuqiu.cc' + url
             # write写函数原型 sheet.write(i,j,'...')
             # i 行号，j 列号，''里面是要写的内容
-            if game_properties == '英超':             # 英超赛事
+            if game_properties == '英超':  # 英超赛事
                 if home_team == mpage_team:
-                    sheet.write(0,0,"英超-"+mpage_team+"-主")
-                    sheet.write(tr_list.index(i)+1, 0, next_url)
-                    sheet.col(0).width = 10000       # 设置一下单元格宽度（没什么卵用
+                    sheet.range(1, 1).value = "英超主场"
+                    sheet.range(tr_list.index(i) + 1, 1).value = next_url
                 else:
-                    sheet.write(0,1,"英超-"+mpage_team+"-客")
-                    sheet.write(tr_list.index(i)+1, 1, next_url)
-                    sheet.col(1).width = 10000
+                    sheet.range(1, 2).value = "英超客场"
+                    sheet.range(tr_list.index(i) + 1, 2).value = next_url
 
-            elif game_properties == '欧冠':           # 欧冠赛事
+            elif game_properties == '欧冠':  # 欧冠赛事
                 if home_team == mpage_team:
-                    sheet.write(0,2,"欧冠-"+mpage_team+"-主")
-                    sheet.write(tr_list.index(i)+1, 2, next_url)
-                    sheet.col(2).width = 10000
+                    sheet.range(1, 3).value = "欧冠主场"
+                    sheet.range(tr_list.index(i) + 1, 3).value = next_url
                 else:
-                    sheet.write(0,3,"欧冠-"+mpage_team+"-客")
-                    sheet.write(tr_list.index(i)+1, 3, next_url)
-                    sheet.col(3).width = 10000
+                    sheet.range(1, 4).value = "欧冠客场"
+                    sheet.range(tr_list.index(i) + 1, 4).value = next_url
 
-            else:                                    # 其他赛事
+            else:  # 其他赛事
                 if home_team == mpage_team:
-                    sheet.write(0,4,"其他赛事-"+mpage_team+"-主")
-                    sheet.write(tr_list.index(i)+1, 4, next_url)
-                    sheet.col(4).width = 10000
+                    sheet.range(1, 5).value = "其他赛事主场"
+                    sheet.range(tr_list.index(i) + 1, 5).value = next_url
                 else:
-                    sheet.write(0,5,"其他赛事-"+mpage_team+"-客")
-                    sheet.write(tr_list.index(i)+1, 5, next_url)
-                    sheet.col(5).width = 10000
+                    sheet.range(1, 6).value = "其他赛事客场"
+                    sheet.range(tr_list.index(i) + 1, 6).value = next_url
 
-        os.remove(filepath)
-        wb.save(filepath)
+        wb.save()
+        wb.close()
+        app.quit()
